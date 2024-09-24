a49d3c94295c24e2199f82de0d2d50bf9aeccb97
DilrubaShondha <dilrubaeasmin2867@gmail.com>
2024-09-25 00:46:04 +0600

    home & blog updates  commit

--- function.js
+++ function.js
@@ -1,3 +1,9 @@
+// blogpage to homepage redirection
 document.getElementById('home-btn').addEventListener('click', function () {
     window.location.href = "./index.html";
+})
+// homepage to blogpage redirection
+document.getElementById('blog-btn').addEventListener('click', function () {
+    console.log('clicked')
+    window.location.href = "./blog.html";
 })
