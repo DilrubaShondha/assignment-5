022a258ed088fa60aacc32ecd7c67930fb066459
DilrubaShondha <dilrubaeasmin2867@gmail.com>
2024-09-25 12:30:47 +0600

    some js functionality updates  commit

--- function.js
+++ function.js
@@ -1,9 +1,39 @@
-// blogpage to homepage redirection
-document.getElementById('home-btn').addEventListener('click', function () {
-    window.location.href = "./index.html";
-})
-// homepage to blogpage redirection
-document.getElementById('blog-btn').addEventListener('click', function () {
-    console.log('clicked')
-    window.location.href = "./blog.html";
-})
+// get inner text value
+function getTextValue(id) {
+    const target = document.getElementById(id).innerText;
+    return parseFloat(target);
+}
+// get input value
+function getValue(id) {
+    const target = document.getElementById(id).value;
+    return parseFloat(target);
+}
+// history card
+function history(amountId, id, text) {
+    const div = document.createElement('div');
+    div.style.border = "1px solid gray"
+    div.style.padding = "32px"
+    div.style.borderRadius = "16px"
+    div.style.marginTop = "20px"
+    const date = new Date();
+    div.innerHTML = `<h1 class="text-lg lg:text-xl font-bold">${getValue(amountId)} Taka is ${text}. </h1><p class="text-textColor">Date: ${date} </p>`
+    document.getElementById(id).appendChild(div);
+}
+// total donate sections function
+function donate(amount, totalDonationAmount, titleId) {
+    const inputValue = getValue(amount);
+    const donateAmount = getTextValue(totalDonationAmount);
+    const donateTitle = document.getElementById(titleId).innerText;
+    const totalRequiredBalance = getTextValue('required-balance');
+    if (inputValue < 1 || isNaN(inputValue)) {
+        alert(`sorry sir you can't donate less than zero amount`);
+        return document.getElementById(amount).value = '';
+    }
+    else {
+        const modal = document.getElementById('my_modal').showModal();
+        const historySection = history(amount, 'history-section', donateTitle);
+        return document.getElementById(totalDonationAmount).innerText = donateAmount + inputValue, document.getElementById('required-balance').innerText = totalRequiredBalance - inputValue, document.getElementById(amount).value = '', modal, historySection;
+    }
+}
+
+
